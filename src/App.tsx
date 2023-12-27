00e6c59a3b731cbfd329b2396f11e93e0e3da220
Serhii Brekhov <bruksbond@gmail.com>
2023-12-27 14:59:54 +0200

    add rtk

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,26 @@ import './App.css';
 import { useAppDispatch, useAppSelector } from './hooks/redux'
 import { userSlice } from './store/reducers/UserSlice'
 import { fetchUsers } from './store/reducers/ActionCreators'
+import PostContainer from './components/PostContainer'
 
 function App() {
-   const {users, isLoading, error} = useAppSelector(state => state.userReducer)
-  // const {increment} = userSlice.actions
-  const dispatch = useAppDispatch()
+  // const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+  // const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    dispatch(fetchUsers())
-  }, [])
+  // useEffect(() => {
+  //   dispatch(fetchUsers())
+  // }, [])
 
   return (
     <div className="App">
-      {isLoading && <h1>Идет загрузка...</h1>}
-        {error && <h1>{error}</h1>}
-        {JSON.stringify(users, null, 2)}
-      </div>
+      {/* {isLoading && <h1>Идет загрузка...</h1>} */}
+      {/* {error && <h1>{error}</h1>} */}
+      {/* {JSON.stringify(users, null, 2)} */}
+      <div style={{display: 'flex'}}>
+            <PostContainer/>
+            {/* <PostContainer2/> */}
+        </div>
+    </div>
   );
 }
 
